ee9b3adb5eff4e420b5d93f6c3dc422afc9a0639
lmiranda1607 <luizm@live.it>
2026-04-02 16:58:48 +0100

    Adicionar arquivos de projeto.

---
+++ TesteImpressao/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TesteImpressao
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TesteImpressao/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace TesteImpressao
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
